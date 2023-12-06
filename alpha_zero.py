ced4836e8f1ada07932d401937aea7ba0fef098f
Yi Dong <yidong@nvidia.com>
2023-12-06 09:13:51 -0800

    show progress
    Signed-off-by: Yi Dong <yidong@nvidia.com>

--- alpha_zero.py
+++ alpha_zero.py
@@ -4,6 +4,7 @@ import random
 import torch
 import torch.nn.functional as F
 from tqdm import trange
+import tqdm
 
 
 class AlphaZeroParallel:
@@ -29,9 +30,17 @@ class AlphaZeroParallel:
         
         # play each of the spg to the end 
         count = 0
+        # add a progress bar to show the progress of the self play
+        total_stes = self.game.row_count * self.game.column_count - 4
+        # only show the progress bar for rank 0
+        if self.model.device == torch.device('cuda:0'):
+            pb = tqdm.tqdm(total=total_stes, desc='Self Play') 
         while len(spGames) > 0:
-            print(f'played {count} steps, {len(spGames)}')
             count += 1
+            if self.model.device == torch.device('cuda:0'):
+                pb.update(1)
+                # show number os spGames left in the progress bar
+                pb.set_postfix({'spGames': len(spGames)})
             # spg.state is on the perspective of player 1 or -1, not the neutral perspective
             states = np.stack([spg.state for spg in spGames])
 
